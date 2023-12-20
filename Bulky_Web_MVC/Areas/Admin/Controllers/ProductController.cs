316bdf3524e43ed5be6a5d5a9efb047e2c3ef3f0
selviarul1612 <selviarul1612@gmail.com>
2023-12-20 22:09:15 +0530

    Creating 'Edit and Delete' View for Product

--- Bulky_Web_MVC/Areas/Admin/Controllers/ProductController.cs
+++ Bulky_Web_MVC/Areas/Admin/Controllers/ProductController.cs
@@ -33,6 +33,62 @@ namespace Bulky_Web_MVC.Areas.Admin.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? categoryFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb12 = _db.Categories.Where(u => u.Id ==  id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.save();
+                TempData["Success"] = "Category Updated successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound(id);
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.save();
+            TempData["Success"] = "Category Deleted successfully";
+            return RedirectToAction("Index");
+        }
+
 
 
     }
